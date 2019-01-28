65ac2179da2dd3b8e8a8287238995913b5fc82a0
Ruben Bartelink <ruben@bartelink.com>
2019-01-28 15:13:18 +0000

    Initial implementation

---
+++ equinox-projector-cosmos/Consumer/Infrastructure.fs
@@ -0,0 +1,47 @@
+﻿[<AutoOpen>]
+module private ProjectorTemplate.Consumer.Infrastructure
+
+open System
+open System.Threading
+open System.Threading.Tasks
+
+type Async with
+    static member AwaitTaskCorrect (task : Task<'T>) : Async<'T> =
+        Async.FromContinuations <| fun (k,ek,_) ->
+            task.ContinueWith (fun (t:Task<'T>) ->
+                if t.IsFaulted then
+                    let e = t.Exception
+                    if e.InnerExceptions.Count = 1 then ek e.InnerExceptions.[0]
+                    else ek e
+                elif t.IsCanceled then ek (TaskCanceledException("Task wrapped with Async has been cancelled."))
+                elif t.IsCompleted then k t.Result
+                else ek(Exception "invalid Task state!"))
+            |> ignore
+
+    static member AwaitTaskCorrect (task : Task) : Async<unit> =
+        Async.FromContinuations <| fun (k,ek,_) ->
+            task.ContinueWith (fun (t:Task) ->
+                if t.IsFaulted then
+                    let e = t.Exception
+                    if e.InnerExceptions.Count = 1 then ek e.InnerExceptions.[0]
+                    else ek e
+                elif t.IsCanceled then ek (TaskCanceledException("Task wrapped with Async has been cancelled."))
+                elif t.IsCompleted then k ()
+                else ek(Exception "invalid Task state!"))
+            |> ignore
+
+type SemaphoreSlim with
+    /// F# friendly semaphore await function
+    member semaphore.Await(?timeout : TimeSpan) = async {
+        let! ct = Async.CancellationToken
+        let timeout = defaultArg timeout Timeout.InfiniteTimeSpan
+        let task = semaphore.WaitAsync(timeout, ct) 
+        return! Async.AwaitTaskCorrect task
+    }
+
+    /// Throttling wrapper which waits asynchronously until the semaphore has available capacity
+    member semaphore.Throttle(workflow : Async<'T>) : Async<'T> = async {
+        let! _ = semaphore.Await()
+        try return! workflow
+        finally semaphore.Release() |> ignore
+    }
