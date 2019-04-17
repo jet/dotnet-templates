3c2670b1e59e3b81fd3c2bf24d09f711b2b6f9fb
Ruben Bartelink <ruben@bartelink.com>
2019-04-17 09:47:44 +0100

    Add PurchaseOrder restriction

--- equinox-sync/Sync/Program.fs
+++ equinox-sync/Sync/Program.fs
@@ -813,7 +813,8 @@ let main argv =
                 || e.EventStreamId.StartsWith("$") 
                 || e.EventType.StartsWith("compacted",StringComparison.OrdinalIgnoreCase)
                 || e.EventStreamId.EndsWith("_checkpoints")
-                || e.EventStreamId = "ReloadBatchId"
+                || e.EventStreamId = "ReloadBatchId" // does not start at 0
+                || e.EventStreamId = "PurchaseOrder-5791" // Too large
                 || e.EventStreamId.EndsWith("_checkpoint")
                 || not (catFilter e.EventStreamId) -> None
             | e -> EventStoreSource.tryToBatch e
