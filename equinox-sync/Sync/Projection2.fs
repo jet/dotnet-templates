0154d8a1e859a53742d9441de80ef96c0c311c6c
Ruben Bartelink <ruben@bartelink.com>
2019-05-10 15:18:17 +0100

    Fix

--- equinox-sync/Sync/Projection2.fs
+++ equinox-sync/Sync/Projection2.fs
@@ -192,7 +192,7 @@ module Scheduling =
             log.Information("Cycles {cycles}/{fullCycles} {@states} Projecting {busy}/{processors} Completed {completed} Exceptions {exns}",
                 !cycles, !fullCycles, states.StatsDescending, used, maxDop, !resultCompleted, !resultExn)
             cycles := 0; fullCycles := 0; states.Clear(); resultCompleted := 0; resultExn:= 0
-            log.Information("Ingestions {batches} {streams:n0}s {events:n0}-{skipped:n0}e Merged {merges} Pending {pending}",
+            log.Information("Ingestions {batches}b {streams:n0}s {events:n0}-{skipped:n0}e Merged {merges} Pending {pending}",
                 !batchesPended, !streamsPended, !eventsSkipped + !eventsPended, !eventsSkipped, !merges, pendingCount)
             batchesPended := 0; streamsPended := 0; eventsSkipped := 0; eventsPended := 0; merges := 0
         abstract member Handle : InternalMessage<'R> -> unit
