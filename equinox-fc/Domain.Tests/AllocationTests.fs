d68c643303a91b9100800e4fa268501e6c51dca8
Ruben Bartelink <ruben@bartelink.com>
2019-12-07 12:19:54 +0000

    Formatting consistency

--- equinox-fc/Domain.Tests/AllocationTests.fs
+++ equinox-fc/Domain.Tests/AllocationTests.fs
@@ -5,6 +5,6 @@ open FsCheck.Xunit
 open Swensen.Unquote
 
 let [<Property>] ``codec can roundtrip`` event =
-    let ee = Events.codec.Encode(None,event)
+    let ee = Events.codec.Encode(None, event)
     let ie = FsCodec.Core.TimelineEvent.Create(0L, ee.EventType, ee.Data)
     test <@ Some event = Events.codec.TryDecode ie @>
--- equinox-fc/Domain/ListAllocation.fs
+++ equinox-fc/Domain/ListAllocation.fs
@@ -8,15 +8,15 @@ type Service(maxListLen, allocators : Allocator.Service, allocations : Allocatio
         let cutoff = let now = DateTimeOffset.UtcNow in now.Add transactionTimeout
         let! state = allocators.Commence(allocatorId, allocationId, cutoff)
         // TODO cancel timed out conflicting work
-        let! (_,state) = allocations.Sync(allocationId, Seq.empty, Allocation.Commence tickets)
+        let! _, state = allocations.Sync(allocationId, Seq.empty, Allocation.Commence tickets)
         return state }
 
     member __.Read(allocationId) : Async<_> = async {
-        let! (_,state) = allocations.Sync(allocationId, Seq.empty, Allocation.Command.Apply ([],[]))
+        let! _, state = allocations.Sync(allocationId, Seq.empty, Allocation.Command.Apply ([], []))
         // TODO incorporate allocator state
         return state }
 
-    member __.Cancel(allocatorId,allocationId) : Async<_> = async {
-        let! (_,state) = allocations.Sync(allocationId, Seq.empty, Allocation.Command.Cancel)
+    member __.Cancel(allocatorId, allocationId) : Async<_> = async {
+        let! _, state = allocations.Sync(allocationId, Seq.empty, Allocation.Command.Cancel)
         // TODO propagate to allocator with reason
         return state }
