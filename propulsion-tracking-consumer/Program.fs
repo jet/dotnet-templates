1c026e08a89dd35d6d37a33d1354e89e1bbb2522
Ruben Bartelink <ruben@bartelink.com>
2020-02-09 22:10:25 +0000

    Remove extraneous pipe in message template

--- propulsion-tracking-consumer/Program.fs
+++ propulsion-tracking-consumer/Program.fs
@@ -120,7 +120,7 @@ module Logging =
             |> fun c -> if verbose then c.MinimumLevel.Debug() else c
             |> fun c -> let theme = Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code
                         if not verbose then c.WriteTo.Console(theme=theme)
-                        else c.WriteTo.Console(theme=theme, outputTemplate="[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}|{Properties}{NewLine}{Exception}")
+                        else c.WriteTo.Console(theme=theme, outputTemplate="[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties}{NewLine}{Exception}")
             |> fun c -> c.CreateLogger()
 
 let [<Literal>] AppName = "ConsumerTemplate"
