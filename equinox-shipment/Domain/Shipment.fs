1e51c9b9e6b9a0f4eacc38319b896a6082044400
kimserey <kimserey.lam@gmail.com>
2020-03-10 18:28:49 +0000

    Shipment

--- equinox-shipment/Domain/Shipment.fs
+++ equinox-shipment/Domain/Shipment.fs
@@ -1,5 +1,49 @@
-﻿namespace Domain
+﻿module Shipment
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type Shipment = { id: string; containerId: string }
+
+module Events =
+
+    let [<Literal>] CategoryId = "Shipment"
+
+    let (|ForClientId|) (clientId: string) = FsCodec.StreamName.create CategoryId clientId
+
+    type Event =
+        | ShipmentCreated  of shipmentId  : string
+        | ShipmentAssigned of containerId : string
+        | Snapshotted      of Shipment
+        interface TypeShape.UnionContract.IUnionContract
+
+    let codec = FsCodec.NewtonsoftJson.Codec.Create<Event>()
+
+module Fold =
+
+    type State = Shipment
+
+    let initial: State = { id = null; containerId = null }
+
+    let evolve (state: State) (event: Events.Event): State =
+        match event with
+        | Events.Snapshotted      snapshot    -> snapshot
+        | Events.ShipmentCreated  shipmentId  -> { state with id = shipmentId }
+        | Events.ShipmentAssigned containerId -> { state with containerId = containerId }
+
+
+    let fold: State -> Events.Event seq -> State =
+        Seq.fold evolve
+
+    let isOrigin (event: Events.Event) =
+        match event with
+        | Events.Snapshotted _ -> true
+        | _ -> false
+
+    let snapshot state = Events.Snapshotted state
+
+type Command =
+    | Create of shipmentId  : string
+    | Assign of containerId : string
+
+let interpret (command: Command) (state: Fold.State): Events.Event list =
+    match command with
+    | Create shipmentId  -> [ Events.ShipmentCreated  shipmentId  ]
+    | Assign containerId -> [ Events.ShipmentAssigned containerId ]
