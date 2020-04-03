49f3ad263970f567d969ebd49045b7dfee3f60b8
Wantastic <wantastic@gmail.com>
2020-04-03 19:34:05 -0400

    Add MissingArg exception catch in main inner try block (#61)

--- propulsion-reactor/Program.fs
+++ propulsion-reactor/Program.fs
@@ -667,7 +667,8 @@ let main argv =
 #endif
             try Configuration.initialize ()
                 if run args then 0 else 3
-            with e -> Log.Fatal(e, "Exiting"); 2
+            with Args.MissingArg msg -> eprintfn "%s" msg; 1
+                | e -> Log.Fatal(e, "Exiting"); 2
         finally Log.CloseAndFlush()
     with Args.MissingArg msg -> eprintfn "%s" msg; 1
         | :? Argu.ArguParseException as e -> eprintfn "%s" e.Message; 1
