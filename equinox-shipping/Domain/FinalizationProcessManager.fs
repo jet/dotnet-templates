10e1c6f8e7963f22fc56a59e2a86f6f933588d68
Ruben Bartelink <ruben@bartelink.com>
2020-04-09 08:33:42 +0100

    Tidy Process Manager

--- equinox-shipping/Domain/FinalizationProcessManager.fs
+++ equinox-shipping/Domain/FinalizationProcessManager.fs
@@ -1,27 +1,32 @@
 module Shipping.Domain.FinalizationProcessManager
 
-type Service(transactions : FinalizationTransaction.Service, containers : Container.Service, shipments : Shipment.Service, maxDop) =
+type Service
+    (   transactions : FinalizationTransaction.Service,
+        containers : Container.Service,
+        shipments : Shipment.Service,
+        /// Maximum parallelism factor when fanning out Assign or Revoke work across Shipments
+        maxDop) =
 
     let execute (transactionId : TransactionId) : FinalizationTransaction.Events.Event option -> Async<bool> =
         let rec loop (update: FinalizationTransaction.Events.Event option) = async {
             let loop event = loop (Some event)
 
-            let! action = transactions.Apply(transactionId, update)
+            let! next = transactions.Apply(transactionId, update)
 
-            match action with
+            match next with
             | FinalizationTransaction.Action.AssignShipments (containerId, shipmentIds) ->
                 let tryAssign sId = async {
                     let! res = shipments.TryAssign(sId, containerId)
                     return if res then None else Some sId
                 }
-                let work = shipmentIds |> Seq.map tryAssign
-                let! failedAssignments = Async.Parallel(work, maxDop)
 
-                let failures = Array.choose id failedAssignments
-                if Array.isEmpty failures then
+                let! assigmentOutcomes = Async.Parallel(seq { for sId in shipmentIds -> tryAssign sId }, maxDop)
+
+                match Array.choose id assigmentOutcomes with
+                | [||] ->
                     return! loop FinalizationTransaction.Events.AssignmentCompleted
-                else
-                    let inDoubt = shipmentIds |> Array.except failures
+                | failedShipmentAssignments ->
+                    let inDoubt = shipmentIds |> Array.except failedShipmentAssignments
                     return! loop (FinalizationTransaction.Events.RevertCommenced {| shipmentIds = inDoubt |})
 
               | FinalizationTransaction.Action.FinalizeContainer containerId ->
@@ -37,10 +42,14 @@ type Service(transactions : FinalizationTransaction.Service, containers : Contai
         }
         loop
 
-    // Transaction ID can be a md5 hash of all shipments ID
-    member __.TryFinalize(transactionId : TransactionId, containerId, shipmentIds) =
-        let initialReq = FinalizationTransaction.Events.FinalizationRequested {| containerId = containerId; shipmentIds = shipmentIds |}
-        execute transactionId (Some initialReq)
+    // Caller can generate the TransactionId via a deterministic hash of the shipmentIds
+    member __.TryFinalize(transactionId, containerId, shipmentIds) : Async<bool> =
+        let initialRequest =
+            FinalizationTransaction.Events.FinalizationRequested
+                {|  containerId = containerId
+                    shipmentIds = shipmentIds |}
+        execute transactionId (Some initialRequest)
 
+    /// Used by watchdog service to drive processing to a conclusion where a given request was orphaned
     member __.Drive(transactionId : TransactionId) =
         execute transactionId None
