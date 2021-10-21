71d36aa2f87bc25a24f8600830fa3d4f85c8d187
Ruben Bartelink <ruben@bartelink.com>
2021-10-21 11:56:13 +0100

    Extract createCategorySnapshotted

--- equinox-shipping/Domain/Types.fs
+++ equinox-shipping/Domain/Types.fs
@@ -29,12 +29,12 @@ module Config =
 
     module Category =
 
-        let create codec initial fold accessStrategy (context, cache) =
-            let cacheStrategy = Equinox.CosmosStore.CachingStrategy.SlidingWindow (cache, System.TimeSpan.FromMinutes 20.)
-            Equinox.CosmosStore.CosmosStoreCategory(context, codec, fold, initial, cacheStrategy, accessStrategy)
-
         let createMemory codec initial fold store =
             Equinox.MemoryStore.MemoryStoreCategory(store, codec, fold, initial)
+
+        let private create codec initial fold accessStrategy (context, cache) =
+            let cacheStrategy = Equinox.CosmosStore.CachingStrategy.SlidingWindow (cache, System.TimeSpan.FromMinutes 20.)
+            Equinox.CosmosStore.CosmosStoreCategory(context, codec, fold, initial, cacheStrategy, accessStrategy)
 
         let createSnapshotted codec initial fold (isOrigin, toSnapshot) (context, cache) =
             let accessStrategy = Equinox.CosmosStore.AccessStrategy.Snapshot (isOrigin, toSnapshot)
