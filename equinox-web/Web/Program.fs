4f9bc502eb0c915b53535d236eee1acc66fae583
Ruben Bartelink <ruben@bartelink.com>
2021-11-15 12:01:10 +0000

    Tidy formatting

--- equinox-web/Web/Program.fs
+++ equinox-web/Web/Program.fs
@@ -9,7 +9,7 @@ type Logging() =
 
     [<System.Runtime.CompilerServices.Extension>]
     static member Configure(c : LoggerConfiguration, appName) =
-        let customTags = ["app",appName]
+        let customTags = ["app", appName]
         c
             .MinimumLevel.Debug()
             .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
@@ -31,7 +31,5 @@ let [<Literal>] AppName = "TodoBackendTemplate"
 let main argv =
     try Log.Logger <- LoggerConfiguration().Configure(AppName).CreateLogger()
         try createWebHostBuilder(argv).Build().Run(); 0
-        with e ->
-            Log.Fatal(e, "Application Startup failed")
-            1
+        with e -> Log.Fatal(e, "Application Startup failed"); 1
     finally Log.CloseAndFlush()
