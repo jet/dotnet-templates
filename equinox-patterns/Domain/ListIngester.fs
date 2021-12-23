b7ffa7884f17e363a0c5fe2583054744ba04b14a
Ruben Bartelink <ruben@bartelink.com>
2021-12-23 21:03:33 +0000

    Tweak naming

--- equinox-patterns/Domain/ListIngester.fs
+++ equinox-patterns/Domain/ListIngester.fs
@@ -1,18 +1,18 @@
-module Patterns.Domain.List
+module Patterns.Domain.ListIngester
 
-type IngestionService internal (tip : ExactlyOnceIngester.Service<_, _, _, _>) =
+type Service internal (tip : ExactlyOnceIngester.Service<_, _, _, _>) =
 
     member _.IngestItems(originEpochId, items : ItemId[]) =
         tip.IngestMany(originEpochId, items)
 
 module Config =
 
-    let createIngester_ linger maxItemsPerEpoch store =
+    let create_ linger maxItemsPerEpoch store =
         let series = ListSeries.Config.create store
         let epochs = ListEpoch.Config.create maxItemsPerEpoch store
         let tip = ExactlyOnceIngester.create linger (series.ReadIngestionEpochId, series.MarkIngestionEpochId) (epochs.Ingest, Array.toSeq)
-        IngestionService(tip)
-    let createIngester store =
+        Service(tip)
+    let create store =
         let defaultLinger = System.TimeSpan.FromMilliseconds 200.
         let maxItemsPerEpoch = 10_000
-        createIngester_ defaultLinger maxItemsPerEpoch store
+        create_ defaultLinger maxItemsPerEpoch store
