5e7e40e36beee7545b56a0da0549b0d5b22e50ef
Ruben Bartelink <ruben@bartelink.com>
2022-09-12 18:28:33 +0100

    Remove dead helpers

--- equinox-web-csharp/Domain/Infrastructure.cs
+++ equinox-web-csharp/Domain/Infrastructure.cs
@@ -1,65 +1,7 @@
-using Equinox;
-using Equinox.Core;
-using Microsoft.FSharp.Core;
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace TodoBackendTemplate
 {
-    public class Accumulator<TEvent,TState>
-    {
-        readonly Func<TState, IEnumerable<TEvent>, TState> _fold;
-        readonly TState _state;
-        public List<TEvent> Accumulated { get; } = new List<TEvent>();
-
-        public Accumulator(Func<TState,IEnumerable<TEvent>,TState> fold, TState state)
-        {
-            _fold = fold;
-            _state = state;
-        }
-
-        public TState State => _fold(_state,Accumulated);
-
-        public void Execute(Func<TState, IEnumerable<TEvent>> f) => Accumulated.AddRange(f(State));
-    }
-
-    public class EquinoxStream<TEvent, TState> : DeciderCore<TEvent, TState>
-    {
-        private readonly Func<TState, IEnumerable<TEvent>, TState> _fold;
-
-        public EquinoxStream(
-                Func<TState, IEnumerable<TEvent>, TState> fold,
-                IStream<TEvent, TState> stream)
-            : base(stream)
-        {
-            _fold = fold;
-        }
-
-        /// Run the decision method, letting it decide whether or not the Command's intent should manifest as Events
-        public async Task<Unit> Execute(Func<TState, IEnumerable<TEvent>> interpret)
-        {
-            return await Transact(interpret);
-        }
-
-        /// Execute a command, as Decide(Action) does, but also yield an outcome from the decision
-        public async Task<T> Decide<T>(Func<Accumulator<TEvent, TState>, T> decide)
-        {
-            (T, IEnumerable<TEvent>) decideWrapped(TState state)
-            {
-                var a = new Accumulator<TEvent, TState>(_fold, state);
-                var r = decide(a);
-                return (r, a.Accumulated);
-            }
-
-            return await Transact(decide: decideWrapped);
-        }
-
-        // Project from the synchronized state, without the possibility of adding events that Decide(Func) admits
-        public async Task<T> Query<T>(Func<TState, T> project) =>
-            await base.Query(project);
-    }
-
     /// System.Text.Json implementation of IEncoder that encodes direct to a UTF-8 Buffer
     public class SystemTextJsonUtf8Codec
     {
