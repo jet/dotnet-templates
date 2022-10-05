09a2665df2b22d5db5f4fedc495676ecb31b7266
Ruben Bartelink <ruben@bartelink.com>
2022-10-05 17:14:47 +0100

    Fix Lambda.CDK arg parsing

--- equinox-shipping/Watchdog.Lambda.Cdk/Program.fs
+++ equinox-shipping/Watchdog.Lambda.Cdk/Program.fs
@@ -12,12 +12,11 @@ let main _ =
     let app = App(null)
 
     // TODO probably replace with Logic to look it up in SSM Parameter Store
-    let codePath, topicArn =
-        match app.Node.TryGetContext codePathArg, app.Node.TryGetContext topicNameArg with
-        | :? string as sa, (:? string as tn) when sa <> null && tn <> null -> sa, tn
+    let codePath, topicArn, storeTableName, indexTableName =
+        match app.Node.TryGetContext codePathArg, app.Node.TryGetContext topicNameArg, app.Node.TryGetContext storeTableNameArg, app.Node.TryGetContext indexTableNameArg with
+        | :? string as sa, (:? string as tn), (:? string as st), (:? string as it)
+            when sa <> null && tn <> null && st <> null && it <> null -> sa, tn, st, it
         | _ -> failwith $"Please supply -c {topicNameArg}=<SNS Topic Arn> -c {indexTableNameArg}=<Index Table Name> -c {storeTableNameArg}=<Store Table Name> and -c {codePathArg}=<Code Path>"
-    let indexTableName = "equinox-test-index"
-    let storeTableName = "equinox-test"
     let _watchdogLambda = WatchdogLambdaStack(app, "Watchdog", WatchdogLambdaStackProps(
         topicArn, indexTableName, storeTableName,
         codePath, "Shipping Watchdog", "Watchdog.Lambda::Shipping.Watchdog.Lambda.Function::Handle"))
