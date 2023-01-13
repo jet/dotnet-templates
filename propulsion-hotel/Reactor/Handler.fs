bd117fedc6963b146452fc92e08c9db23bc57ce8
Ruben Bartelink <ruben@bartelink.com>
2023-01-13 13:35:46 +0000

    Explain OverrideWritePosition more thoroughly

--- propulsion-hotel/Reactor/Handler.fs
+++ propulsion-hotel/Reactor/Handler.fs
@@ -34,13 +34,28 @@ let private isReactionStream = function
     | GroupCheckout.Category -> true
     | _ -> false
 
+// Invocation of the handler is prompted by event notifications from the event store's feed.
+// Wherever possible, multiple events get processed together (e.g. in catchup scenarios or where the async checkpointing
+//   had not yet managed to mark a new high water mark on the feed before the Reactor got shut down etc)
+// Where a handler fails with an exception, the next invocation will also include any further events for that stream that
+//   may have arrived since the last handler invocation took place
+// NOTE while Propulsion supplies the handler with the full set of outstanding events since the last successful checkpoint,
+//   the nature of the reaction processing we are performing here can also be reliant on state that's inferred based on events 
+//   prior to those that will have arrived on the feed. For that reason, the caller does not forward the `events` argument here.
 let private handle (processor : GroupCheckoutProcess.Service) stream = async {
     match stream with
     | GroupCheckout.StreamName groupCheckoutId ->
         let! outcome, ver' = processor.React(groupCheckoutId)
-        // Checkpointing happens asynchronously. When the process is started up we might reprocess
-        // already handled messages. The service returns the version of the process manager stream
-        // so we can use that to fast-forward our processing and avoid re-triggering actions unnecessarily
+        // For the reasons noted above, processing is carried out based on the reading of the stream for which the handler was triggered.
+        // In some cases, due to the clustered nature of the store (and not requiring consistent reads / leader connections
+        //   / using session consistency), the set of events read from the stream may not actually include the prompting event. 
+        // As a result, we have the Reaction logic inform us of the observed version of the stream, and declare that version
+        //   as the position attained on this stream as part of this handler invocation.
+        // This, in the case where the read did noy include the prompting event, it'll remain buffered externally, and the handler will
+        //   be re-triggered immediately until such time as the observed position on the stream is at or beyond that of the buffered events
+        // NOTE also that in some cases, the observed position on the stream can be beyond that which has been notified via
+        //   the change feed. In those cases, Propulsion will drop any incoming events that would represent duplication of processing,
+        //   (and not even invoke the Handler unless one or more of the feed events are beyond the write position)
         return struct (Propulsion.Streams.SpanResult.OverrideWritePosition ver', outcome)
     | other ->
         return failwithf "Span from unexpected category %A" other }
